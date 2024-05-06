a1ff6b0495f5f6e92644589774134c3535cb06bf
Hans Oksendahl <hansoksendahl@gmail.com>
2024-05-06 14:20:24 -0700

    testing if build is required

--- packages/@hox.io/components/src/index.js
+++ packages/@hox.io/components/src/index.js
@@ -1,10 +1,10 @@
-export {default as Flex} from './flex';
-export {default as Column} from './column';
-export {default as Row} from './row';
-export {default as Spacing} from './spacing';
-export {default as Text} from './text';
-export {default as InputBlob} from './inputBlob';
-export {default as InputSelect} from './inputSelect';
-export {default as InputText} from './inputText';
-export {default as Icon} from './Icon';
-export * from './constants';
+export { default as Column } from './column'
+export * from './constants'
+export { default as Flex } from './flex'
+export { default as Icon } from './icon'
+export { default as InputBlob } from './inputBlob'
+export { default as InputSelect } from './inputSelect'
+export { default as InputText } from './inputText'
+export { default as Row } from './row'
+export { default as Spacing } from './spacing'
+export { default as Text } from './text'
